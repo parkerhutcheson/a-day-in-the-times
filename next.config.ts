b82912b79257e33f18468acb56a5964206ae80e5
parkerhutcheson <pdhutcheson@gmail.com>
2025-12-04 17:52:04 +0900

    Whitelisting the nyt static image domain

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'developer.nytimes.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'static01.nyt.com',
+      }
     ],
   },
   // hostname: 'https://developer.nytimes.com/files/poweredby_nytimes_200c.png?v=1583354208354',
